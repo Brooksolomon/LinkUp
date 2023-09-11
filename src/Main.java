35ad7878affe67e46f331d596ea9c2e88d0771f5
Brook Solomon <officialbrooksolo@gmail.com>
2023-09-11 12:00:15 +0300

    First commit again

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,7 @@ public class Main {
         // Press Alt+Enter with your caret at the highlighted text to see how
         // IntelliJ IDEA suggests fixing it.
         System.out.printf("Hello and welcome!");
+        System.out.println("Cheching");
 
         // Press Shift+F10 or click the green arrow button in the gutter to run the code.
         for (int i = 1; i <= 5; i++) {
